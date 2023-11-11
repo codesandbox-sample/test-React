6922dd1ac12e8d44d1bec8bdf6c0f4f1041b069c
css <148056189+codesandbox-sample@users.noreply.github.com>
2023-11-11 06:55:42 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const App = () => {
-  return (
-    // JSX記法：javascriptの中にhtmlタグを書く
-    // <div></div> return htmlの内容は一つのタグで括る必要あり
-    // <React.Fragment></React.Fragment>  この記載でも可
-    <>
-      <h1>こんにちは</h1>
-      <p>お元気ですか？</p>
-    </>
-  );
-};
+// JSX記法：javascriptの中にhtmlタグを書く
+// const App = () => {
+//   return (
+//     // <div></div> return htmlの内容は一つのタグで括る必要あり
+//     // <React.Fragment></React.Fragment>  この記載でも可
+//     <>
+//       <h1>こんにちは</h1>
+//       <p>お元気ですか？</p>
+//     </>
+//   );
+// };
+
+import App from "./App";
+// コンポーネント
+// 命名規則：「パスカルケース」先頭が大文字で始まり、単語の区切りを大文字とする変数名の付け方
 
 ReactDom.render(<App />, document.getElementById("root"));
